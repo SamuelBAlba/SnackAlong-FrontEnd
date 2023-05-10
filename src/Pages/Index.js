2fcd7eb6234d43a15ba98774c08a00838c1c1d0a
SamuelBAlba <brayanbalba24@gmail.com>
2023-05-10 18:31:19 -0400

    snack

--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -1,9 +1,11 @@
-
+import Snacks from "../Components/Snacks";
 
 function Index() {
     return (
         <div className="Index">
             <h1>Index</h1>
+            <Snacks/>
+            
         </div>
     )
 }
